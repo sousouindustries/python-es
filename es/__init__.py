b800638f656d90729e489b52d72c00d832b34e3e
Cochise Ruhulessin <cochiseruhulessin@gmail.com>
2016-01-12 09:42:40 +0100

    Added setup.py

--- es/__init__.py
+++ es/__init__.py
@@ -3,3 +3,7 @@ from es.event import Event
 from es.publisher import publishes
 from es.publisher import handles
 from es.aggregate import Aggregate
+
+
+def get_version():
+    return '1.0.0alpha1'
---
+++ setup.py
@@ -0,0 +1,55 @@
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import find_packages, setup
+
+MODULE_NAME = 'es'
+MODULE_DESC = "A Python Event-Sourcing (ES) framework."
+
+SETUP_DIR = abspath(dirname(__file__))
+README_FILE = join(SETUP_DIR, 'README.rst')
+REQUIREMENTS = abspath(join(dirname(__file__), 'requirements.txt'))
+EXCLUDE_FROM_PACKAGES = []
+MODULE_LONG = None
+if os.path.exists(README_FILE):
+    MODULE_LONG = open(README_FILE).read()
+
+
+# Dynamically calculate the version based,
+version = __import__(MODULE_NAME).get_version()
+install_requires = []
+
+
+# Get the requirements from requirements.txt
+if os.path.exists(REQUIREMENTS):
+    with open(REQUIREMENTS) as f:
+        install_requires = list(filter(bool, f.read().splitlines()))
+
+
+setup(
+    name=MODULE_NAME,
+    version=version,
+    url='https://www.sousouindustries.com/',
+    author='Cochise Ruhulessin',
+    author_email='cochise.ruhulessin@sousouindustries.com',
+    description=MODULE_DESC,
+    long_description=MODULE_LONG,
+    license='Apache Licence 2.0',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
